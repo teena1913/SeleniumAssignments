f744f551538380b9457140ec574d2ffacfda20a5
teena1913 <124455513+teena1913@users.noreply.github.com>
2023-02-15 17:56:28 +0530

    teena1913 /week2day2

--- week2/day2.assignments/CreateContact.java
+++ week2/day2.assignments/CreateContact.java
@@ -4,50 +4,35 @@ public class CreateContact {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * //Pseudo Code
-		 * 
-		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
-		 * 
-		 * 2. Enter UserName and Password Using Id Locator
-		 * 
-		 * 3. Click on Login Button using Class Locator
-		 * 
-		 * 4. Click on CRM/SFA Link
-		 * 
-		 * 5. Click on contacts Button
-		 * 
-		 * 6. Click on Create Contact
-		 *  
-		 * 7. Enter FirstName Field Using id Locator
-		 * 
-		 * 8. Enter LastName Field Using id Locator
-		 * 
-		 * 9. Enter FirstName(Local) Field Using id Locator
-		 * 
-		 * 10. Enter LastName(Local) Field Using id Locator
-		 * 
-		 * 11. Enter Department Field Using any Locator of Your Choice
-		 * 
-		 * 12. Enter Description Field Using any Locator of your choice 
-		 * 
-		 * 13. Enter your email in the E-mail address Field using the locator of your choice
-		 * 
-		 * 14. Select State/Province as NewYork Using Visible Text
-		 * 
-		 * 15. Click on Create Contact
-		 * 
-		 * 16. Click on edit button 
-		 * 
-		 * 17. Clear the Description Field using .clear
-		 * 
-		 * 18. Fill ImportantNote Field with Any text
-		 * 
-		 * 19. Click on update button using Xpath locator
-		 * 
-		 * 20. Get the Title of Resulting Page.
-
-         */
+		
+	
+		  ChromeDriver driver=new ChromeDriver(); 
+	    driver.get("http://leaftaps.com/opentaps/contol/login");
+            driver.manage().window().maximize();
+            driver.findElement(By.id("username")).sendKeys("demoSalesManager");
+            driver.findElement(By.id("password")).sendKeys("crmsfa");
+            driver.findElement(By.className("decorativeSubmit")).click();
+            driver.findElement(By.linkText("CRM/SFA")).click();
+	    driver.findElement(By.className("x-panel-header")).click();
+	    driver.findElement(By.linkText("Create Contact")).click();
+	    driver.findElement(By.id("firstNameField")).sendKeys("Infanteena");
+            driver.findElement(By.id("lastNameField")).sendKeys("A");
+            driver.findElement(By.id("createContactForm_firstNameLocal")).sendKeys("Infanteena");
+	    driver.findElement(By.id("createContactForm_lastNameLocal")).sendKeys("A");
+            driver.findElement(By.id("createContactForm_departmentName")).sendKeys("ECE");
+            driver.findElement(By.id("createContactForm_description")).sendKeys("hi this is me");
+            driver.findElement(By.id("createContactForm_primaryEmail")).sendKeys("infanteena123@gmail.com");
+            WebElement selectprovince = driver.findElement(By.id("createContactForm_generalStateProvinceGeoId"));
+            Select op=new Select(selectprovince);
+            op.selectByVisibleText("New York");
+	    driver.findElement(By.className("smallSubmit")).click();
+	    driver.findElement(By.linkText("subMenuButton")).click();
+            driver.findElement(By.id("updateContactForm_description")).clear();
+	    driver.findElement(By.id("updateContactForm_importantNote")).sendKeys("goodmorning");
+	    driver.findElement(By.xpath("//input[@class='smallSubmit']")).click();
+            driver.findElement(By.className("smallSubmit")).click();
+	    System.out.println("the tile is "+ driver.getTitle());
+		 
 		
 	}
 
